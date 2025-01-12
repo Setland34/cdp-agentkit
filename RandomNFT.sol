14f8c0083067a658c94ceba9179a006b8bd54c92
Seth Landry <waymakerijn@outlook.com>
2025-01-11 22:34:44 -0600

    Refactor `RandomNFT.sol` to use `ERC721Enumerable` and improve minting process
    * **Imports**
      - Replace `ERC721` with `ERC721Enumerable`
      - Add `Ownable` import
    
    * **Constructor**
      - Update constructor to accept `merkleRoot` parameter
    
    * **Minting**
      - Rename `_tokenIds` to `_tokenIdCounter`
      - Update `mintNFT` function to `mint` and use `_tokenIdCounter`
      - Add `_baseURI` function for metadata URL
    
    * **Token URI**
      - Update `tokenURI` function to use `_baseURI`

--- RandomNFT.sol
+++ RandomNFT.sol
@@ -1,42 +1,44 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract RandomNFT is ERC721, Ownable {
+contract RandomNFT is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIdCounter;
 
     bytes32 public merkleRoot;
 
-    constructor() ERC721("RandomNFT", "RNFT") {}
-
-    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+    constructor(bytes32 _merkleRoot) ERC721("RandomNFT", "RNFT") {
         merkleRoot = _merkleRoot;
     }
 
-    function mintNFT(address recipient, bytes32[] calldata merkleProof) public returns (uint256) {
-        require(_verify(_leaf(recipient), merkleProof), "Invalid Merkle Proof");
-
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        return newItemId;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://api.example.com/metadata/";
     }
 
-    function _leaf(address recipient) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(recipient));
+    function mint(bytes32[] calldata _merkleProof) public {
+        require(_verify(_leaf(msg.sender), _merkleProof), "Invalid Merkle Proof");
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function _leaf(address account) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(account));
     }
 
     function _verify(bytes32 leaf, bytes32[] memory proof) internal view returns (bool) {
         return MerkleProof.verify(proof, merkleRoot, leaf);
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return string(abi.encodePacked("https://api.example.com/metadata/", Strings.toString(tokenId)));
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
     }
 }
